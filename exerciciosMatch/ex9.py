2231fcfa477b5d2783c5a298246771d856ee78b3
GoncaloMatos <gcanteiro2002@gmail.com>
2025-11-12 22:45:37 +0000

    bora

--- exerciciosMatch/ex9.py
+++ exerciciosMatch/ex9.py
@@ -1,2 +1,13 @@
 metodo = input("Introduz o método: ")
 conteudo = input("Introduz o conteudo: ")
+requisicao = {"metodo": metodo, "conteudo":conteudo}
+
+match requisicao:
+    case{"metodo": "GET", "conteudo": _}:
+        print("Requisição GET recebida")
+    case{"metodo": "POST", "conteudo": c} if c!="":
+        print("Requisição POST com dados válidos")
+    case{"metodo": "POST", "conteudo": c} if c =="":
+        print("Requisição POST sem dados")
+    case _:
+        print("Método não suportado")       
